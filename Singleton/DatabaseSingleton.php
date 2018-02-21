2a47743a6194005fa225fc1d5726befee79e004c
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2018-02-21 15:05:05 -0300

    FEA: Criação do exemplo de singleton

---
+++ Singleton/DatabaseSingleton.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Singleton;
+
+/**
+ * Implementação da classe de conexão com o padrão "Singleton"
+ *
+ * Class DatabaseSingleton
+ * @package Singleton
+ */
+class DatabaseSingleton extends \PDO
+{
+    /**
+     * DatabaseSingleton constructor.
+     * @param $dsn
+     * @param $username
+     * @param $passwd
+     * @param $options
+     */
+    private function __construct($dsn, $username, $passwd, $options)
+    {
+        parent::__construct($dsn, $username, $passwd, $options);
+    }
+
+    /**
+     * DatabaseSingleton constructor.
+     * @param $dsn
+     * @param $username
+     * @param $password
+     * @param $options
+     */
+    public static function getInstance(
+        string $dsn,
+        string $username,
+        string $password,
+        array $options = []
+    ) {
+        parent::__construct($dsn, $username, $password, $options)
+    }
+}
