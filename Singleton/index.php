962c0dc2b23bb3552c2f6bc0d978e8dfb26d9117
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2018-02-22 11:32:34 -0300

    FIX: Alteração do exemplo de singleton
    Agora ele se fundamente em cima de uma classe de logs.

--- Singleton/index.php
+++ Singleton/index.php
@@ -2,6 +2,13 @@
 
 require_once('../autoloader.php');
 
+use Singleton\LogsSingleton;
+
+$logsSingleton = LogsSingleton::obterInstancia();
+
+print_r($logsSingleton->gravarLog(['test', 'fsaafs']));
+
+die;
 $instanciaDoBancoDeDados = \Singleton\DatabaseSingleton::getInstance();
 
 $segundaInstanciaDoBancoDeDados = \Singleton\DatabaseSingleton::getInstance();
