d447a9c958d58583cf3723e29846385f8927beff
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2018-02-23 14:38:06 -0300

    FEA: Arquivo de teste do facade.

--- Facade/index.php
+++ Facade/index.php
@@ -2,3 +2,13 @@
 
 require_once('../autoloader.php');
 
+$bibliotecaFacade = new \Facade\BibliotecaFacade();
+
+$codigoLivro = '123456';
+$cpfCliente = '12345678998';
+
+$efetuouRetirada = $bibliotecaFacade->efetuaRetirada($codigoLivro, $cpfCliente);
+
+if ($efetuouRetirada) {
+    $bibliotecaFacade->disparaMensagens($codigoLivro, $cpfCliente, 'CHAVE_API', 'SENHA_API');
+}
