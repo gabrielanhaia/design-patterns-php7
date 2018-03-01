1de7a8085a9ae1fea0d562bcdfdd2c5bfdf23df0
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2018-03-01 10:28:17 -0300

    STY: Alteração do cliente de exemplo do Facade

--- Facade/ModuloControleClientes.php
+++ Facade/ModuloControleClientes.php
@@ -18,9 +18,9 @@ class ModuloControleClientes
     {
         // Simula a busca de um cliente pelo seu CPF.
         return [
-            'nome' => 'Cliente de Teste',
+            'nome' => 'Gabriel Anhaia',
             'telefone' => '99999999999',
-            'email' => 'cliente@teste_facade.com'
+            'email' => 'gabriel@mestredev.com.br'
         ];
     }
 }
