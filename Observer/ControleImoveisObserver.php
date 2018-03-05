406e14d7eb8d24deb0b9e7d93bd85cf8bde60066
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2018-03-05 17:53:22 -0300

    FEA: Criação do observer concreto de controle de imóveis disponíveis

---
+++ Observer/ControleImoveisObserver.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Observer;
+
+/**
+ * Class ControleImoveisObserver
+ * @package Observer
+ */
+class ControleImoveisObserver implements Observer
+{
+
+    public function atualizado(string $codigo, float $valor)
+    {
+        // TODO: Implement atualizado() method.
+    }
+}
