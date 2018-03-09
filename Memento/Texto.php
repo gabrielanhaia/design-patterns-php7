865e8f4f4094c4dcbbd3f891a59f2c5a2f8ce07b
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2018-03-09 14:40:14 -0300

    FEA: Implementação da classe de texto do Memento

---
+++ Memento/Texto.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Memento;
+
+/**
+ * Class Texto
+ * @package Memento
+ * @author Gabriel Anhaia <gabriel@mestredev.com.br>
+ */
+class Texto
+{
+    /** @var string $texto Texto do editor. */
+    protected $texto;
+
+    /** @var TextoCareTaker $textoCareTaker Armazenador dos estados de texto. */
+    protected $textoCareTaker;
+
+    /**
+     * Texto constructor.
+     */
+    public function __construct()
+    {
+        $this->textoCareTaker = new TextoCareTaker();
+        $this->texto = '';
+    }
+
+    /**
+     * Adiciona texto a string atual.
+     * @param string $texto
+     */
+    public function escreverTexto(string $texto)
+    {
+        $this->textoCareTaker->adicionarMemento(new TextoMemento($texto));
+        $this->texto .= $texto
+    }
+
+    /**
+     * Desfaz a ultima atualização no texto.
+     */
+    public function desfazerEscrita()
+    {
+        $this->texto = $this
+            ->textoCareTaker
+            ->retornarUltimoEstadoSalvo()
+            ->getTexto();
+    }
+
+    /**
+     * Retorna o texto atual.
+     * @return string
+     */
+    public function retornarTexto(): string
+    {
+       return $this->texto;
+    }
+}
