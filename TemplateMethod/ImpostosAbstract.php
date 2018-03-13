c7c93ec84db9170cb9c0fd4896adb79e92e942ef
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2018-03-13 17:39:34 -0300

    FEA(Template Method): Implementação dos métodos da classe abstrata

--- TemplateMethod/ImpostosAbstract.php
+++ TemplateMethod/ImpostosAbstract.php
@@ -12,4 +12,54 @@ namespace TemplateMethod;
  */
 abstract class ImpostosAbstract
 {
+    /**
+     * Implementação do "Template Method".
+     *
+     * @param float $precoProduto
+     * @return float
+     */
+    public function calculaImpostosProduto(float $precoProduto): float
+    {
+        $precoProduto = $this->calculaPrimeiroImpostoFixo($precoProduto);
+
+        $precoProduto = $this->calculaSegundoImpostoFixo($precoProduto);
+
+        $precoProduto = $this->calculaImpostoVariavel($precoProduto);
+
+        return $precoProduto;
+    }
+
+    /**
+     * @param float $precoProduto
+     * @return float
+     */
+    abstract protected function calculaImpostoVariavel(float $precoProduto): float;
+
+    /**
+     * @param float $precoProduto
+     * @return float
+     */
+    private function calculaPrimeiroImpostoFixo(float $precoProduto): float
+    {
+        return ($precoProduto * 1.3);
+    }
+
+    /**
+     * @param float $precoProduto
+     * @return float
+     */
+    private function calculaSegundoImpostoFixo(float $precoProduto): float
+    {
+        return ($precoProduto * 1.1);
+    }
+
+    /**
+     * Implementação do "Hook Method"
+     * @param float $precoProduto
+     * @return float
+     */
+    protected function calculaImpostoAdicional(float $precoProduto): float
+    {
+        return $precoProduto - ($precoProduto * 0.1);
+    }
 }
