c495cade3bd00aaf752cfd3339f58243259d28c7
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2018-03-16 15:20:48 -0300

    FEA(Strategy): Teste da implementação do Strategy

--- Strategy/CompressaoContext.php
+++ Strategy/CompressaoContext.php
@@ -12,10 +12,27 @@ class CompressaoContext
     private $compressaoStrategy;
 
     /**
+     * CompressaoContext constructor.
+     * @param CompressaoStrategy $compressaoStrategy
+     */
+    public function __construct(CompressaoStrategy $compressaoStrategy)
+    {
+        $this->compressaoStrategy = $compressaoStrategy;
+    }
+
+    /**
      * @param CompressaoStrategy $compressaoStrategy
      */
     public function setCompressaoStrategy(CompressaoStrategy $compressaoStrategy)
     {
         $this->compressaoStrategy = $compressaoStrategy;
+    }
+
+    /**
+     * Método responsável por executar a estratégia de compressão.
+     */
+    public function comprimir(string $caminhoArquivo)
+    {
+        $this->compressaoStrategy->comprimir($caminhoArquivo)
     }
 }
--- Strategy/index.php
+++ Strategy/index.php
@@ -1,1 +1,19 @@
 <?php
+
+require_once('../autoloader.php');
+
+$strategyRar = new \Strategy\ComprimirRar();
+$strategyZip = new \Strategy\ComprimirZip();
+$strategyTar = new \Strategy\ComprimirTar();
+
+$compressaoContext = new \Strategy\CompressaoContext($strategyRar);
+
+$compressaoContext->comprimir('/CAMINHO/ARQUIVOS');
+echo "\n";
+
+$compressaoContext->setCompressaoStrategy($strategyZip);
+$compressaoContext->comprimir('/CAMINHO/ARQUIVOS');
+echo "\n";
+
+$compressaoContext->setCompressaoStrategy($strategyTar);
+$compressaoContext->comprimir('/CAMINHO/ARQUIVOS');
