42720c2c039e48d2c469518b3c157cf836c01341
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2018-03-24 14:42:17 -0300

    FEA(Decorator): Alteração no teste do Decorator.

--- Decorator/index.php
+++ Decorator/index.php
@@ -4,10 +4,10 @@ require_once('../autoloader.php');
 
 $decorator = new \Decorator\Mago();
 $decorator = new \Decorator\AnelDeFogo($decorator);
-/*$decorator = new \Decorator\AnelDeFogo($decorator);
+$decorator = new \Decorator\AnelDeFogo($decorator);
 $decorator = new \Decorator\ColarDaForca($decorator);
 $decorator = new \Decorator\EspadaMagica($decorator);
-*/
+
 echo $decorator->getNome();
 echo "\n";
 echo "Ataque = {$decorator->getAtaque()}";
