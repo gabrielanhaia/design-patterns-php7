0003269641148a4cdfaf9dd7b098a77554e1c70a
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2018-03-24 13:30:06 -0300

    FIX(Decorator): Ajustes no Decorator

--- Decorator/PersonagemDecorator.php
+++ Decorator/PersonagemDecorator.php
@@ -2,22 +2,35 @@
 
 namespace Decorator;
 
-
+/**
+ * Class PersonagemDecorator
+ * @package Decorator
+ */
 abstract class PersonagemDecorator extends Personagem
 {
     /** @var Personagem $personagem */
     protected $personagem;
 
+    /**
+     * PersonagemDecorator constructor.
+     * @param Personagem $personagem
+     */
     public function __construct(Personagem $personagem)
     {
         $this->personagem = $personagem;
     }
 
+    /**
+     * @return string
+     */
     public function getNome(): string
     {
         return $this->personagem->getNome();
     }
 
+    /**
+     * @return float
+     */
     public function getAtaque(): float
     {
         return $this->personagem->getAtaque() + $this->ataque;
--- Decorator/index.php
+++ Decorator/index.php
@@ -2,12 +2,12 @@
 
 require_once('../autoloader.php');
 
-$decorator = new \Decorator\PersonagemDecorator(new \Decorator\Mago());
+$decorator = new \Decorator\Mago();
 $decorator = new \Decorator\AnelDeFogo($decorator);
-$decorator = new \Decorator\AnelDeFogo($decorator);
+/*$decorator = new \Decorator\AnelDeFogo($decorator);
 $decorator = new \Decorator\ColarDaForca($decorator);
 $decorator = new \Decorator\EspadaMagica($decorator);
-
+*/
 echo $decorator->getNome();
 echo "\n";
 echo "Ataque = {$decorator->getAtaque()}";
